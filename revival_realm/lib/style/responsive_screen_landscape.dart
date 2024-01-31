a16dba412ede45dc869e34a3115de80ed36887b2
Seah Jia Jun <orangepufflejj@gmail.com>
2024-02-01 01:41:24 +0800

    Change menu items to be below title

--- revival_realm/lib/style/responsive_screen_landscape.dart
+++ revival_realm/lib/style/responsive_screen_landscape.dart
@@ -31,57 +31,44 @@ class ResponsiveScreen extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constraints) {
         // This widget wants to fill the whole screen.
+        // final size = constraints.biggest;
         final size = constraints.biggest;
         final padding = EdgeInsets.all(size.shortestSide / 30);
 
         
         // "Landscape" / "tablet" mode.
-        final isLarge = size.width > 900;
+        // final isLarge = size.width > 900;
 
-        return Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              flex: isLarge ? 7 : 5,
-              child: SafeArea(
-                right: false,
+        return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SafeArea(
+                bottom: false,
+                child: Padding(
+                  padding: padding,
+                  child: topMessageArea,
+                ),
+              ),
+              Expanded(
+                child: SafeArea(
+                  top: false,
+                  bottom: false,
+                  minimum: padding,
+                  child: squarishMainArea,
+                ),
+              ),
+              SafeArea(
+                top: false,
                 maintainBottomViewPadding: true,
-                minimum: padding,
-                child: squarishMainArea,
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Column(
-                children: [
-                  SafeArea(
-                    bottom: false,
-                    left: false,
-                    maintainBottomViewPadding: true,
-                    child: Padding(
-                      padding: padding,
-                      child: topMessageArea,
-                    ),
+                child: Padding(
+                  padding: padding,
+                  child: Center(
+                    child: rectangularMenuArea,
                   ),
-                  Expanded(
-                    child: SafeArea(
-                      top: false,
-                      left: false,
-                      maintainBottomViewPadding: true,
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Padding(
-                          padding: padding,
-                          child: rectangularMenuArea,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
+                ),
               ),
-            ),
-          ],
-        );
+            ],
+          );
         }
     );
   }
