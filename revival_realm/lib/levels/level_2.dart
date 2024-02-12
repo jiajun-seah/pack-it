f54be2c2f0f01e2b962a5656dd307ca6197cde6d
Seah Jia Jun <orangepufflejj@gmail.com>
2024-02-12 11:06:04 +0800

    initialize level 2, upload new assets

---
+++ revival_realm/lib/levels/level_2.dart
@@ -0,0 +1,314 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+// import '../audio/audio_controller.dart';
+// import '../audio/sounds.dart';
+import '../game_internals/level_state.dart';
+import '../level_selection/levels.dart';
+// import '../play_session/pixel.dart';
+// import '../play_session/foods_mapping.dart';
+import '../play_session/food.dart';
+
+/// This widget defines the game UI itself, without things like the settings
+/// button or the back button.
+class LevelTwo extends StatelessWidget {
+  const LevelTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final level = context.watch<GameLevel>();
+    final levelState = context.watch<LevelState>();
+    
+    return Column(
+        children: [
+          Text('Level ${level.id}'),
+          Expanded(
+            child: Row(children: [
+              Flexible(
+                flex: 1,
+                child: Center(
+                  child: AspectRatio(
+                    aspectRatio: 1/2,
+                    child: Stack (
+                      children: [
+                        Container(
+                          alignment: Alignment.topCenter,
+                          child: FractionallySizedBox(
+                            heightFactor: 0.5,
+                            child: Container(
+                              color: Colors.black12,
+                              child: LayoutBuilder(
+                                builder: (context, constraints) => Visibility(
+                                  visible: !levelState.checkDropped('banana'),
+                                  child: Draggable<String>(
+                                    data: 'banana',
+                                    feedback: Container(
+                                      width: constraints.maxWidth*1.05,
+                                      child: Food(
+                                      name: 'banana',
+                                      relativeWidth: 1.0,
+                                      relativeHeight: 0.5,
+                                      sprite: Image.asset('assets/images/foods/Banana.gif')
+                                    )),
+                                    childWhenDragging: Container(),
+                                    child: Food(
+                                      name: 'banana',
+                                      relativeWidth: 1.0,
+                                      relativeHeight: 0.5,
+                                      sprite: Image.asset('assets/images/foods/Banana.gif')
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomCenter,
+                          child: FractionallySizedBox(
+                            heightFactor: 0.5,
+                            child: Container(
+                              color: Colors.black12,
+                              child: LayoutBuilder(
+                                builder: (context, constraints) => Visibility(
+                                  visible: !levelState.checkDropped('orange_l'),
+                                  child: Draggable<String>(
+                                    data: 'orange',
+                                    feedback: Container(
+                                      width: constraints.maxWidth,
+                                      child: Food(
+                                      name: 'sandwich_bl',
+                                      relativeWidth: 0.5,
+                                      relativeHeight: 0.5,
+                                      sprite: Image.asset('assets/images/foods/Orange_r.gif')
+                                    )),
+                                    childWhenDragging: Container(),
+                                    child: Food(
+                                      name: 'sandwich_bl',
+                                      relativeWidth: 0.5,
+                                      relativeHeight: 0.5,
+                                      sprite: Image.asset('assets/images/foods/Orange_r.gif')
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],)
+                  ),
+                ),
+              ),
+              Flexible(
+                flex:2,
+                child: Center(
+                  child: AspectRatio(
+                    aspectRatio: 1/1,
+                    child: Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.topLeft,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.5,
+                            heightFactor: 0.5,
+                            child: DragTarget<String>(
+                              builder: (
+                                BuildContext context,
+                                List<dynamic> accepted,
+                                List<dynamic> rejected,
+                              ) {
+                                debugPrint(levelState.progress.toString());
+                                return Container(
+                                  // color: Colors.blue,
+                                  child: Image.asset(levelState.checkDropped('sandwich_tl')
+                                    ? 'assets/images/foods/sandwich_tl.gif'
+                                    : 'assets/images/foods/Grey_L.png')
+                                );
+                              },
+                              onWillAccept: (data) {
+                                return data == 'sandwich_tl';
+                              },
+                              onAccept: (data) {
+                                levelState.setProgress('sandwich_tl');
+                                levelState.evaluate();
+                                debugPrint(levelState.progress.toString());
+                              },
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.topRight,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.5,
+                            heightFactor: 0.5,
+                            child: DragTarget<String>(
+                              builder: (
+                                BuildContext context,
+                                List<dynamic> accepted,
+                                List<dynamic> rejected,
+                              ) {
+                                debugPrint(levelState.progress.toString());
+                                return Container(
+                                  // color: Colors.blue,
+                                  child: Image.asset(levelState.checkDropped('sandwich_tr')
+                                    ? 'assets/images/foods/sandwich_tr.gif'
+                                    : 'assets/images/foods/Grey_L.png')
+                                );
+                              },
+                              onWillAccept: (data) {
+                                return data == 'sandwich_tr';
+                              },
+                              onAccept: (data) {
+                                levelState.setProgress('sandwich_tr');
+                                levelState.evaluate();
+                                debugPrint(levelState.progress.toString());
+                              },
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomLeft,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.5,
+                            heightFactor: 0.5,
+                            child: DragTarget<String>(
+                              builder: (
+                                BuildContext context,
+                                List<dynamic> accepted,
+                                List<dynamic> rejected,
+                              ) {
+                                debugPrint(levelState.progress.toString());
+                                return Container(
+                                  // color: Colors.blue,
+                                  child: Image.asset(levelState.checkDropped('sandwich_bl')
+                                    ? 'assets/images/foods/sandwich_bl.gif'
+                                    : 'assets/images/foods/Grey_L.png')
+                                );
+                              },
+                              onWillAccept: (data) {
+                                return data == 'sandwich_bl';
+                              },
+                              onAccept: (data) {
+                                levelState.setProgress('sandwich_bl');
+                                levelState.evaluate();
+                                debugPrint(levelState.progress.toString());
+                              },
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomRight,
+                          child: FractionallySizedBox(
+                            widthFactor: 0.5,
+                            heightFactor: 0.5,
+                            child: DragTarget<String>(
+                              builder: (
+                                BuildContext context,
+                                List<dynamic> accepted,
+                                List<dynamic> rejected,
+                              ) {
+                                debugPrint(levelState.progress.toString());
+                                return Container(
+                                  // color: Colors.blue,
+                                  child: Image.asset(levelState.checkDropped('sandwich_br')
+                                    ? 'assets/images/foods/sandwich_br.gif'
+                                    : 'assets/images/foods/Grey_L.png')
+                                );
+                              },
+                              onWillAccept: (data) {
+                                return data == 'sandwich_br';
+                              },
+                              onAccept: (data) {
+                                levelState.setProgress('sandwich_br');
+                                levelState.evaluate();
+                                debugPrint(levelState.progress.toString());
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Center(
+                  child: AspectRatio(
+                    aspectRatio: 1/2,
+                    child: Stack (
+                      children: [
+                        Container(
+                          alignment: Alignment.topCenter,
+                          child: FractionallySizedBox(
+                            heightFactor: 0.5,
+                            child: Container(
+                              color: Colors.black12,
+                              child: LayoutBuilder(
+                                builder: (context, constraints) => Visibility(
+                                  visible: !levelState.checkDropped('sandwich_br'),
+                                  child: Draggable<String>(
+                                    data: 'sandwich_br',
+                                    feedback: Container(
+                                      width: constraints.maxWidth,
+                                      child: Food(
+                                      name: 'sandwich_br',
+                                      relativeWidth: 0.5,
+                                      relativeHeight: 0.5,
+                                      sprite: Image.asset('assets/images/foods/sandwich_br.gif')
+                                    )),
+                                    childWhenDragging: Container(),
+                                    child: Food(
+                                      name: 'sandwich_br',
+                                      relativeWidth: 0.5,
+                                      relativeHeight: 0.5,
+                                      sprite: Image.asset('assets/images/foods/sandwich_br.gif')
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.bottomCenter,
+                          child: FractionallySizedBox(
+                            heightFactor: 0.5,
+                            child: Container(
+                              color: Colors.black12,
+                              child: LayoutBuilder(
+                                builder: (context, constraints) => Visibility(
+                                  visible: !levelState.checkDropped('sandwich_tl'),
+                                  child: Draggable<String>(
+                                    data: 'sandwich_tl',
+                                    feedback: Container(
+                                      width: constraints.maxWidth,
+                                      child: Food(
+                                      name: 'sandwich_tl',
+                                      relativeWidth: 0.5,
+                                      relativeHeight: 0.5,
+                                      sprite: Image.asset('assets/images/foods/sandwich_tl.gif')
+                                    )),
+                                    childWhenDragging: Container(),
+                                    child: Food(
+                                      name: 'sandwich_tl',
+                                      relativeWidth: 0.5,
+                                      relativeHeight: 0.5,
+                                      sprite: Image.asset('assets/images/foods/sandwich_tl.gif')
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],)
+                  ),
+                ),
+              ),
+            ],),
+          )
+        ],
+      );
+  }
+}
